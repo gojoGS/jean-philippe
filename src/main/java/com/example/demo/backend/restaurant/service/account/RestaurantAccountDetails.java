f998d5d2c74be2396b01ba1614ccd1060b0e6e44
Bartha Zoltán <barthazoli00@gmail.com>
2022-02-13 20:26:32 +0100

    created restaurant account helper classes

---
+++ src/main/java/com/example/demo/backend/restaurant/service/account/RestaurantAccountDetails.java
@@ -0,0 +1,13 @@
+package com.example.demo.backend.restaurant.service.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class RestaurantAccountDetails {
+    private long id;
+    private String username;
+}
---
+++ src/main/java/com/example/demo/backend/restaurant/service/account/RestaurantAccountService.java
@@ -0,0 +1,7 @@
+package com.example.demo.backend.restaurant.service.account;
+
+public interface RestaurantAccountService {
+    void create(RestaurantAccountDetails restaurant);
+
+    void delete(RestaurantAccountDetails restaurantAccountDetails);
+}
